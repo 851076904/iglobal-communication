6234efb07abd3b07f442c23e454fc043af31e989
851076904@qq.com <851076904@qq.com>
2021-03-21 18:31:45 +0800

    fix ci

--- src/test/java/com/example/demo/TestLog.java
+++ src/test/java/com/example/demo/TestLog.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
 import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
 public class TestLog {
-
     //默认是放在在src目录下
     public static Logger logger  =  Logger.getLogger(TestLog. class );
 
-    public static void main(String[] args){
-
+    @Test
+    public void loggerTest(){
         logger.debug( " debug " );
         logger.error( " error " );
         logger.info( " info" );
